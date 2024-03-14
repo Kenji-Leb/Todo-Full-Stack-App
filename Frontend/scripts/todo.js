f9aebb1ea25495149cd26caace173101289906a0
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-14 04:03:31 +0200

    fixed some indentaion, js, styling.

---
+++ Frontend/scripts/todo.js
@@ -0,0 +1,6 @@
+const addButton = document.getElementById("add-button")
+const deleteButton = document.getElementById("delete-button")
+
+addButton.addEventListener("click", (e) => {
+    console.log("clicked")
+})
