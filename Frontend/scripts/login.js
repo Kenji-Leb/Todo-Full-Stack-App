b23ca9213a60ab94aff8212b257302594062013d
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-15 00:29:05 +0200

    test

--- Frontend/scripts/login.js
+++ Frontend/scripts/login.js
@@ -17,18 +17,19 @@ const inputPassword = document.getElementById("input-password");
 
 const btnLogin = document.getElementById("btn-login");
 
-const users = getUsers()
+// const users = getUsers()
 
 btnLogin.addEventListener("click", (e) => {
-    const user = checkCredentials(inputEmail.value, inputPassword.value);
-    if (user) {
-        localStorage.setItem('signedIn', 'true');
-        if(user.isAdmin){
-            window.location.href = "./pages/todo.htm"
-        }else{
-            window.location.href = "./pages/todo.html";
-        }
-    } else {
-        alert("Invalid username or password.");
-    }
+    window.location.assign("/pages/todo.html")
+    // const user = checkCredentials(inputEmail.value, inputPassword.value);
+    // if (user) {
+    //     localStorage.setItem('signedIn', 'true');
+    //     if(user.isAdmin){
+    //         window.location.href = "./pages/todo.html"
+    //     }else{
+    //         window.location.href = "./pages/todo.html";
+    //     }
+    // } else {
+    //     alert("Invalid username or password.");
+    // }
 });
