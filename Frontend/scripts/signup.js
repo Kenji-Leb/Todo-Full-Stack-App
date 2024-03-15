c4688c42318428da3fab55aacbf957838f91816b
Abbas <45877262+Kenji-Leb@users.noreply.github.com>
2024-03-15 20:00:59 +0200

    test

--- Frontend/scripts/signup.js
+++ Frontend/scripts/signup.js
@@ -11,18 +11,23 @@ btnSignup.addEventListener("click", (e) => {
         password: inputPassword.value,
     };
 
-    fetch('localhost/todo_backend/signup.php', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(user),
+    fetch('http://localhost/todo_backend/signup.php', {
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user),
     })
-    .then(response => response.json())
+    .then(response => {
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    return response.json();
+    })
     .then(data => {
-        console.log(data);
-        localStorage.setItem("currentUser", JSON.stringify(data));
-        window.location = "./login.html";
+    console.log(data);
+    localStorage.setItem("currentUser", JSON.stringify(data));
+    window.location = "./login.html";
     })
     .catch(error => console.error('Error:', error));
 });
